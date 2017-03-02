e58deb326666d281b985a21e90f0787c58026693
sergey-krokhmal <xai.sergey.krokhmal@gmail.com>
2017-03-02 23:52:23 +0300

    Add bootstrap and link it

--- index.php
+++ index.php
@@ -1,2 +1,8 @@
-<?php
-echo 'hello world';
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="assets/libs/bootstrap/css/bootstrap.min.css"/>
+    </head>
+    <body>
+    </body>
+</html>
